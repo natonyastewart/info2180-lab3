4287de4d7c980d5e89106fcf8aa322cf7fcfa758
Natonya Stewart <91899843+natonyastewart@users.noreply.github.com>
2022-10-14 21:59:43 -0500

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,56 @@
+const squares = document.querySelectorAll("board");
+const status = document.getElementById("status");
+const button = document.getElementsByClassName("btn");
 
+const p_X = "X";
+const p_O = "O";
+var playerX = true;
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+function restartGame()
+{ 
+    array_X = [];
+    array_O = [];
+    currentPlays = [];
+
+    stat = document.getElementById("status"); 
+    stat.innerHTML = "Move your mouse over a square and click to play an X or an O.";
+
+    var squares = document.getElementById("board").children; 
+    for (i = 0; i < squares.length; i++)
+    {
+        squares[i].className = "square"; 
+        squares[i].innerHTML = "";
+        squares[i].addEventListener('click',clickFunc,{once : true}); 
+        squares[i].id = i;
+    }
+}
+
+document.addEventListener("DOMContentLoaded",function()
+{
+    restartGame();
+    button[i].addEventListener("click",restartGame);
+})
+
+// finalize conditional statement
+
+function Winner(plays)
+{    
+    console.log(plays);
+    return winningCombinations.some(winCombo => {  
+        return winCombo.every(pos =>{  
+            return plays.includes(pos); 
+                
+        })       
+    }) 
+}
